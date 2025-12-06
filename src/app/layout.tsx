b37e53974db2dcc7f87aa3c653ef673b7d38eb3b
Viren0990 <veerendraverma106@gmail.com>
2025-12-06 13:50:28 +0530

    Initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon-96x96.png' },
-      { url: '/icon.png', sizes: '32x32', type: 'image/png' },
+      { url: '/logo.png', sizes: '32x32', type: 'image/png' },
       { url: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/web-app-manifest-192x192.png', sizes: '512x512', type: 'image/png' },
+      { url: '/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
