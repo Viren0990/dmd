3e684deb8fdc929e8d20a3fd2395a2c709f78bba
Viren0990 <veerendraverma106@gmail.com>
2025-12-06 12:39:20 +0530

    Second Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,13 @@ export const metadata: Metadata = {
   // Icons/Favicons
   icons: {
     icon: [
-      { url: '/favicon.ico' },
+      { url: '/favicon-96x96.png' },
       { url: '/icon.png', sizes: '32x32', type: 'image/png' },
-      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/web-app-manifest-192x192.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
   },
   
